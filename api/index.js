1ad03009a205a4b0a34384e1608ac3ce3ccb6d31
pramodkatta <pramodkatta1@gmail.com>
2024-09-24 15:36:46 +0530

    Connected to data base

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Mongo DB is Connected!");
+    }).catch((err) => {
+        console.log(err)
+    })
+
 
 const app = express();
 
 app.listen(3000, () => {
-        console.log('Server is Running on Port 3000!!!');
+        console.log('Server is Running on Port 3000!');
     }
 );
