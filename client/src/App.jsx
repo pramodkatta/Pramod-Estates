608290fd272d19db1aa898017a3a1b1b596df054
pramodkatta <pramodkatta1@gmail.com>
2024-12-30 20:06:16 +0530

    Update the Header and Make the Profile Page Private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+
+
 
 
 export default function App() {
@@ -15,8 +18,10 @@ export default function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/sign-in" element={<SignIn/>} />
             <Route path="/sign-up" element={<SignUp/>} />
-            <Route path="about" element={<About/>}/>
-            <Route path="profile" element={<Profile/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route element={<PrivateRoute/>}>
+              <Route path="/profile" element={<Profile/>}/>
+            </Route>
         </Routes>
     </BrowserRouter>
   )
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -21,7 +21,7 @@ export default function OAuth() {
                 },
                 body: JSON.stringify({name: result.user.displayName,
                                         email: result.user.email,
-                                        photo: result.user.photoURL
+                                        photo: result.user.photoURL,
                 }),
             });
 
